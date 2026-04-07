68b3fe42988e3897225877972d54d4445b1cf8da
IanTorres01 <ian.torres@utp.ac.pa>
2026-04-06 22:26:55 -0500

    Add files via upload

---
+++ matematicas.php
@@ -0,0 +1,24 @@
+<?php
+
+echo "Hola mundo <br>";
+
+$num1 = 5;
+$num2 = 10;
+
+$suma= $num1+$num2;
+$resta= $num2-$num1;
+$multi= $num1*$num2;
+$division= $num2/$num1;
+$operacion = 5+2*3;
+$precendencia= (5+2)*3;
+
+echo "El resultado de la suma: $suma <br>";
+echo "El resultado de la suma: $resta<br>";
+echo "El resultado de la suma: $multi <br>";
+echo "El resultado de la suma: $division <br>";
+echo "La operacion sin utilizar parentesis da como resultado: $operacion <br>";
+echo "La operacion utilizando parentesis da como resultado: $precendencia <br>";
+
+
+
+?>
